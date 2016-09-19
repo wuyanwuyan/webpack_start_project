549832842ada1dc7cceb1aa0111a0772c24b14f8
wuyanwuyan <xxsinxmu@gmail.com>
2016-09-19 19:16:30 +0800

    CSS路径加载有问题

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,6 @@
  * Created by Administrator on 2016/9/18.
  */
 // entry.js
-require("./style.css") // 载入 style.css
+require("../css/style.css") // 载入 style.css
 document.write('It works111.')
 document.write(require('./module.js')) // 添加模块
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 var webpack = require('webpack')
+var path = require('path')
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+console.log('+++++++++++++++++++++',path)
 
 module.exports = {
-  context: __dirname + "/app",
+  context: __dirname + "/src",
   entry: './main/index.js',
   output: {
-    path: dist,
+    path: "dist",
     filename: 'bundle.js'
   },
+  plugins: [
+    new CommonsChunkPlugin("common.js", ["./libs/bootstrap/js/npm.js"])
+  ],
   module: {
     loaders: [
       {test: /\.css$/, loader: 'style!css'}
     ]
   }
 }
+
