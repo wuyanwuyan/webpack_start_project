32623ee11763663fce35d37d624394dedbe54357
wuyanwuyan <xxsinxmu@gmail.com>
2016-09-23 18:38:54 +0800

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 var webpack = require('webpack')
 var path = require('path')
+
+
+const ROOT_PATH = path.resolve(__dirname)
+const SRC_PATH = path.resolve(ROOT_PATH, 'src')
+const DIST_PATH = path.resolve(ROOT_PATH, 'dist')
+const MODULE_PATH = path.resolve(ROOT_PATH, 'node_modules')
+
+
+
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 console.log('+++++++++++++++++++++',path)
 
 module.exports = {
   context: __dirname + "/src",
-  entry: './main/index.js',
+  entry: {
+    mainPage:'./main/index.js'
+  },
   output: {
-    path: "dist",
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
   },
   plugins: [
     new CommonsChunkPlugin("common.js", ["./libs/bootstrap/js/npm.js"])
@@ -17,6 +28,9 @@ module.exports = {
     loaders: [
       {test: /\.css$/, loader: 'style!css'}
     ]
+  },
+  resolveLoader:  {
+    root: path.join(__dirname, "node_modules") 
   }
 }
 
