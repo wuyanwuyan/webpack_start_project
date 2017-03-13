ccb6e29d9f4f047a31433c88d8c1ca99d6da1fe1
wuyanwuyan <xxsinxmu@gmail.com>
2017-03-13 12:46:18 +0800

    提交小修改

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -36,3 +36,25 @@ $("#contact-way-icon").click(function (event) {
 $('body').on('click',function(){
     $(".real-contact-content").removeClass('is-visible');
 })
+
+$(window).scroll(function(e) {
+    throttle(onScroll);
+});
+
+function onScroll() {
+    var top = $("#contact").offset().top;
+    var wh = $(this).height();
+    var scrollTop = $(this).scrollTop();
+    if(top - scrollTop - wh <= -20) {
+        $("#contact-way-icon").addClass('is-visible');
+        $(window).off('scroll');
+    }
+}
+// 防止scroll 触发太频繁
+function throttle(method, context) {
+    if(method.tId) return;
+    method.tId = setTimeout(function(){
+        method.call(context);
+        method.tId = undefined;
+    }, 100);
+}
